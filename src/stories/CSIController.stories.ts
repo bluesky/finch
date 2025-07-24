6037aa497886f7eb29fccba86df026121ceaaf50
Nicholas M <nmanha@lbl.gov>
2025-07-24 14:52:54 -0700

    added CSIController Story

---
+++ src/stories/CSIController.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import CSIController from '@/components/CSIController/CSIController';
+
+const meta = {
+    title: 'General Components/CSIController',
+    component: CSIController,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    }
+} satisfies Meta<typeof CSIController>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        fileName: "ADBase.adl", 
+        P: "13SIM1", 
+        R: "cam1"
+
+    }
+}
+
