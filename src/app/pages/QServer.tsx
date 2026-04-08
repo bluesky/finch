0f4dab8a17dd33dc7947bf36c24af51771f37668
Seij De Leon <seijdeleon@lbl.gov>
2026-04-08 13:36:21 -0700

    remove unused pages

--- src/app/pages/QServer.tsx
+++
@@ -1,6 +0,0 @@
-import ContainerQServer from "src/components/QServer/ContainerQServer"
-export default function QServer(){
-    return (
-        <ContainerQServer url="http://badurl" className="max-w-[1000px]"/>
-    )
-}
