f11844458fe9ed4188903ea8f3c44b443de348ac
seijdeleon <seijdeleon@lbl.gov>
2026-04-17 10:31:48 -0700

    upgrade to react router v7

--- src/testing/tests/HubMainContent.test.tsx
+++ src/testing/tests/HubMainContent.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
 import HubMainContent from '../../components/HubMainContent';
 import { RouteItem } from '../../types/navigationRouterTypes';
 
--- src/testing/tests/HubSidebar.test.tsx
+++ src/testing/tests/HubSidebar.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
 import HubSidebar from '../../components/HubSidebar';
 import { RouteItem } from '../../types/navigationRouterTypes';
 
